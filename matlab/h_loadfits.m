402e3b6055e63c154fafd5c7fca260b137a52488
everebelyi <akire.v@gmail.com>
2015-05-05 15:27:12 +0200

    Add conditional gunzip and delete(file) lines.

--- matlab/h_loadfits.m
+++ matlab/h_loadfits.m
@@ -3,10 +3,13 @@ function I = h_loadfits(FileName)
 % h_loadfits function
 % Description: Loads a fits image and extracts WCS. Fixes WCS.
 %--------------------------------------------------------------------------
+if ~isempty(strfind(FileName,'.gz')) 
+    unzippedFileName=gunzip(FileName);
+    FileName=char(unzippedFileName);
+end
 
 im = fitsread(FileName, 'image');
 im(~isfinite(im)) = 0;
-%wcs = get_fits_wcs(FileName,[],'image');
 wcs = get_fits_wcs(FileName,[],'Image');
 
 wcs.CUNIT1='deg';
@@ -16,3 +19,7 @@ m = fitsread(FileName, 'image', 2);
 mask = m > 0;
 
 I = struct('Image', im, 'Mask', mask, 'WCS', wcs, 'Color', [100, 100, 100]);
+
+if exist(FileName, 'file') && exist(strcat(FileName, '.gz'), 'file') 
+  delete(FileName);
+end
