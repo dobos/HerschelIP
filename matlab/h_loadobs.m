08dc8c3b19c2d697cd6f05fda09b9889aa3aaa0a
everebelyi <akire.v@gmail.com>
2015-05-12 16:22:17 +0200

    h_imregister -  'Color'  instead of 'R' 'G' 'B' in struct() h_loadobs - conditional filenames (level20/level25/blue1/blue2)

--- matlab/h_loadobs.m
+++ matlab/h_loadobs.m
@@ -1,11 +1,33 @@
 function [A, B, C, D] = h_loadobs( file )
-
     dir = '/Users/Erika/WORK/HerschelPSC/FITS/pacsphoto/';
-    
-    A = h_loadfits(strcat(dir, file, '_js_blue2.fits'));
-    B = h_loadfits(strcat(dir, file, '_js_red.fits'));
-    C = h_loadfits(strcat(dir, file, '_hpf_blue2.fits'));
-    D = h_loadfits(strcat(dir, file, '_hpf_red.fits'));
+
+if ~isempty(strfind(file,'level25')) 
+    FileName=strcat(dir, file, '_js_blue1.fits');
+    if exist(FileName, 'file') 
+        A = h_loadfits(strcat(dir, file, '_js_blue1.fits'));
+        B = h_loadfits(strcat(dir, file, '_js_red.fits'));
+        C = h_loadfits(strcat(dir, file, '_hpf_blue1.fits'));
+        D = h_loadfits(strcat(dir, file, '_hpf_red.fits'));
+    else
+        A = h_loadfits(strcat(dir, file, '_js_blue2.fits'));
+        B = h_loadfits(strcat(dir, file, '_js_red.fits'));
+        C = h_loadfits(strcat(dir, file, '_hpf_blue2.fits'));
+        D = h_loadfits(strcat(dir, file, '_hpf_red.fits'));
+    end
+else    
+    FileName=strcat(dir, file, '_blue1.fits');
+    if exist(FileName, 'file') 
+        A = h_loadfits(strcat(dir, file, '_blue1.fits'));
+        B = h_loadfits(strcat(dir, file, '_red.fits'));
+        C = h_loadfits(strcat(dir, file, '_blue1.fits'));
+        D = h_loadfits(strcat(dir, file, '_red.fits')); 
+    else
+        A = h_loadfits(strcat(dir, file, '_blue2.fits'));
+        B = h_loadfits(strcat(dir, file, '_red.fits'));
+        C = h_loadfits(strcat(dir, file, '_blue2.fits'));
+        D = h_loadfits(strcat(dir, file, '_red.fits')); 
+    end
+end
 
 end
 
